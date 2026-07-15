0f04f3687cdf8c921bdc0d57871942e1c9162753
Harko5 <147317960+Harko5@users.noreply.github.com>
2026-07-15 08:23:12 +0700

    Update SharedDto.java

--- tr/src/main/java/com/example/tr/Model/SharedDto.java
+++ tr/src/main/java/com/example/tr/Model/SharedDto.java
@@ -1,7 +1,20 @@
 package com.example.tr.Model;
 
+import com.example.tr.Mock.DTO.MockOneResponse;
+import com.example.tr.Mock.DTO.MockTwoResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Menyimpan data yang diteruskan antar langkah orchestration.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SharedDto {
-    
+    private MockOneResponse mock1Response;
+    private MockTwoResponse mock2Response;
 }
 // Ini adalah wadah (struktur data/objek) yang akan digunakan 
 // oleh Harta, Christival, dan Yefta untuk saling mengoper 
